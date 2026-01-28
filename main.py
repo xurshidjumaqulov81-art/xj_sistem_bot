571f0a9d54d3c9e4bca344d492549700c0360539
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-01-28 21:20:37 +0400

    Update main.py

--- main.py
+++ main.py
@@ -154,10 +154,14 @@ async def text_handler(message: Message):
             await db.set_state(user_id, DONE)
 
             return await message.answer(
-                "✅ <b>Сиз тўлиқ дарсликни олдингиз!</b>\n\n"
-                "Энди навбатдаги босқичга ўтасиз 👇\n"
-                f"{NEXT_BOT_LINK}"
-            )
+              text = "✅ <b>Сиз тўлиқ дарсликни олдингиз!</b>\n\n"
+
+if NEXT_BOT_LINK:
+    text += "Энди навбатдаги босқичга ўтасиз 👇\n" + NEXT_BOT_LINK
+else:
+    text += "Админ сиз билан боғланади."
+
+await message.answer(text)
 
         await db.set_stage3_idx(user_id, next_idx)
         return await send_stage3_audio(message, user_id, next_idx)
